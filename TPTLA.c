5cea29511956d1ac509e17693507e48fc350d4dc
Federico <federico@federico-MS-7369.(none)>
2012-06-23 03:18:22 -0300

    Metiendo los char*, anda el parseo pero explotan otras funciones

--- TPTLA.c
+++ TPTLA.c
@@ -9,22 +9,13 @@ main(int argc, char* argv[]){
 	int pid=0;
 	char* file= argv[1];
 	char* occur;
-	if((occur=strstr(file,".dot"))!=NULL){
-
+	if((occur=strstr(file,".gr"))!=NULL){
 		pid=fork();
-		if(pid==0)
-			execv("automata", argv);
-		//else
-		//	wait();
-	}
-	else if((occur=strstr(file,".gr"))!=NULL){
-		pid=fork();
-		if(pid==0)
-			execv("gramatica",argv);
-	//	else
-	//		wait();
+		if(pid==0){
+	printf("HOLAAAAAAAAAAAAAAAAAAAAAA");
+			execv("gramatica",argv);}
 	}
 	else
-		printf("El archivo no es ni .gr ni .dot\n");
+		printf("El archivo .gr\n");
 		return 0;
 }
